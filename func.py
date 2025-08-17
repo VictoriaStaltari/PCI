e6a27a8736e932ef3fade621d09b767c5c327679
VictoriaStaltari <166453567+VictoriaStaltari@users.noreply.github.com>
2025-08-17 16:29:04 -0700

    Restore organizar function removed by merge

--- func.py
+++ func.py
@@ -9,3 +9,31 @@ def priorizar_materias(materias_faltantes):
             )
     )
     
+
+
+
+def organizar(nro_materias_cuatrimeste, materias_faltantes):
+    """Organiza las materias faltantes en cuatrimestres de acuerdo al número de materias por cuatrimestre.
+
+    Args:
+        nro_materias_cuatrimeste (int): Número máximo de materias a cursar por cuatrimestre.
+        materias_faltantes (list): Lista de objetos Materia que representan las materias aún no cursadas.
+
+    Returns:
+        tuple: Una tupla que contiene una lista de listas (cada sublista representa un cuatrimestre con materias cursables) y la lista actualizada de materias faltantes.
+    """
+    salida = [[]]  # Lista de cuatrimestres con materias cursables
+    i = 0
+    eliminar_sin_irse_de_rango = []
+    for materia in materias_faltantes:
+        if materia.cursable():
+            eliminar_sin_irse_de_rango.append(materia)
+            if len(salida[i]) < nro_materias_cuatrimeste:
+                salida[i].append(materia)
+            else:
+                i += 1
+                salida.append([materia])
+    for e in eliminar_sin_irse_de_rango:
+        materias_faltantes.pop(materias_faltantes.index(e))
+
+    return salida, materias_faltantes
