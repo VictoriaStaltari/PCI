ea2464eaa5885744f60286c02a4cff0840309173
Victoria Staltari <victoriastaltari19@gmail.com>
2025-08-17 18:52:36 -0300

    comentarios y otros

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
+from clases import Carrera, Materia
+from func import priorizar_materias
 
 app = Flask(__name__)
 
@@ -9,6 +11,9 @@ def home():
 
 @app.route('/carrera/<nombre_carrera>')
 def plan_carrera(nombre_carrera):
+    nombreJSON = f'planes/{nombre_carrera}.json'
+    with open(nombreJSON, 'r') as f:
+        plan_carrera = json.load(f)
     return render_template('carrera.html', carrera=nombre_carrera)
 
 @app.route('/carrera/<nombre_carrera>/planificador')
@@ -18,6 +23,13 @@ def planificador(nombre_carrera):
     
     # Aquí se procesaría la lógica para planificar las materias
     # Por ejemplo, se podría llamar a una función que use las clases Carrera y Materia
+    
+    # 1) Traer el plan
+    # 2) Convertirlo a diccionario y dsp a clases
+    # 3) la lista de materias aprobadas
+    # 4) materias faltantes, lista
+    # 5) ordenar la lista
+    # 6) armar los cuatrimestres
     
     cuatrimestres_organizados = []
     return render_template('planificador.html', cuatrimestres_organizados=cuatrimestres_organizados)
