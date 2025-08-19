9c22654b2db487bca16520d1630c2b3de76000a8
Victoria Staltari <victoriastaltari19@gmail.com>
2025-08-18 21:02:50 -0300

    pase la funcion q estaba en app de cuatris acá para poder llamarla, sino se creaba pero no se ejecutaba

--- func.py
+++ func.py
@@ -37,3 +37,26 @@ def organizar(nro_materias_cuatrimeste, materias_faltantes):
         materias_faltantes.pop(materias_faltantes.index(e))
 
     return salida, materias_faltantes
+
+def ajustar_cuatriimestres(nro_materias_cuatrimeste, materias_faltantes): 
+    i = 0 
+    salida_final = [[]]
+
+    while len(materias_faltantes) > 0: 
+        salida, materias_faltantes = organizar(nro_materias_cuatrimeste, materias_faltantes)  # salida es lista de listas
+        
+        for cuatri in salida:  # 'cuatri' es una lista de materias
+            if i >= len(salida_final):  # Si no existe la sublista, la agregamos
+                salida_final.append([])
+            
+            # Intentamos agregar materias a la sublista actual
+            while cuatri:  # mientras queden materias en esta lista de cuatrimestre
+                espacio = nro_materias_cuatrimeste - len(salida_final[i])
+                if espacio > 0:
+                    # Agregamos hasta llenar el cuatrimestre actual
+                    salida_final[i].extend(cuatri[:espacio])
+                    cuatri = cuatri[espacio:]  # removemos las materias agregadas
+                if len(salida_final[i]) >= nro_materias_cuatrimeste:
+                    i += 1  # pasamos al siguiente cuatrimestre
+
+    return salida_final
